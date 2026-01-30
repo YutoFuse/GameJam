bb3a5e6c22b4ffdba1f00d59ac7af40014e89e0e
LAPTOP-2HAQK071\原田大輔 <h.daisuke0131@icloud.com>
2026-01-30 21:30:44 +0900

    フォルダー、プログラム、シーンの追加

---
+++ Assets/plogramu/field_create.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class field_create : MonoBehaviour
+{
+    int[][] eria = new int[3][];
+    int eriaCount;
+    const int eriaWidth = 0;
+    const int eriaHeight = 0;
+    public GameObject background;
+    public Transform parent;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Vector2 defaultpos = new Vector2(0.0f, 0.0f);
+        defaultpos.x=-(eriaWidth/2);
+        defaultpos.y=-(eriaHeight/2);
+
+        for (int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+
+                Vector2 pos = new Vector2(0.0f, 0.0f);
+                pos.x += i*100f;
+                pos.y += j*100f;
+                GameObject obj;
+                obj = Instantiate(background, parent);
+                obj.transform.position = pos;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
