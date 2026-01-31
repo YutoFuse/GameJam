071757c8b2d49aa0955e2f2ef2bac9eb56a2484b
nosuke0131 <vbdjzmv8sy@privaterelay.appleid.com>
2026-01-31 11:21:02 +0900

    プログラムの追加

--- Assets/plogramu/field_create.cs
+++ Assets/plogramu/field_create.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class field_create : MonoBehaviour
 {
-    int[][] eria = new int[3][];
-    int eriaCount;
-    const int eriaWidth = 0;
-    const int eriaHeight = 0;
+    int[,] eria = new int[3,3];
+    GameObject[,] face=new GameObject[3,3];
+    int eriaCount=0;
+    const int eriaWidth = 3;
+    const int eriaHeight = 3;
+    float cellsize=100f;
     public GameObject background;
     public Transform parent;
 
@@ -13,27 +15,36 @@ public class field_create : MonoBehaviour
     void Start()
     {
         Vector2 defaultpos = new Vector2(0.0f, 0.0f);
-        defaultpos.x=-(eriaWidth/2);
-        defaultpos.y=-(eriaHeight/2);
+        defaultpos.x=-(eriaWidth-1)*cellsize/2f;
+        defaultpos.y=-(eriaHeight - 1) * cellsize / 2f;
 
         for (int i = 0; i < 3; i++)
         {
             for(int j = 0; j < 3; j++)
             {
 
-                Vector2 pos = new Vector2(0.0f, 0.0f);
-                pos.x += i;
-                pos.y += j;
+                Vector2 pos = defaultpos;
+                pos.x += i*cellsize;
+                pos.y += j*cellsize;
                 GameObject obj;
-                obj = Instantiate(background, parent);
-                obj.transform.position = pos;
+                obj = Instantiate(background,parent);
+                obj.transform.localPosition = pos;
+                eria[i,j] = 1;
+                eriaCount++;
+                face[i, j] = obj;
             }
         }
+        Debug.Log("ƒ}ƒX–Ú"+eriaCount);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int i = 1,j = 1;
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("OK");
+            face[i,j].SetActive(false);
+        }
     }
 }
